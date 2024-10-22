97dd4de20664da0445d96cd04e680ed74d747c84
DominicObil <dobioso519@g.educaand.es>
2024-10-22 17:48:49 +0200

    version1

---
+++ src/main/java/org/iesalixar/daw2/dominicobil/dwese_ticket_logger_webapp/Application.java
@@ -0,0 +1,13 @@
+package org.iesalixar.daw2.dominicobil.dwese_ticket_logger_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
